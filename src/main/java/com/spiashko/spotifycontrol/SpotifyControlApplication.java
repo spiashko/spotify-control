234559863d27264e682171274e52de65f443f596
Siarhei Piashko <peshko.sergey@gmail.com>
2021-12-04 21:10:01 +0100

    initial commit

---
+++ src/main/java/com/spiashko/spotifycontrol/SpotifyControlApplication.java
@@ -0,0 +1,13 @@
+package com.spiashko.spotifycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spiashko/spotifycontrol/SpotifyControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiashko.spotifycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
